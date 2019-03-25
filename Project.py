345551a80ddff8c407d720c840c67504613d966a
Alex Cherry <moecherry2@gmail.com>
2019-03-25 19:15:13 +0000

    Update and rename project.py to Project.py

---
+++ Project.py
@@ -0,0 +1,10 @@
+
+#Alex Cherry G00347106 
+#Graph Theory Finite Automata Project
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+  print("Yay! It is Tuesday.")
+else:
+  print("Unfortunately it is not Tuesday.")
