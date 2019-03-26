87d5007d2e51395d93b24aed737d78a125cc4b97
Alex Cherry <moecherry2@gmail.com>
2019-03-26 00:25:48 +0000

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,6 +1,6 @@
-
 #Alex Cherry G00347106 
-#Graph Theory Finite Automata Project
+#Graph Theory Non-Deterministic Finite Automata Project
+
 
 import datetime
 
@@ -8,3 +8,19 @@ if datetime.datetime.today().weekday() == 1:
   print("Yay! It is Tuesday.")
 else:
   print("Unfortunately it is not Tuesday.")
+
+
+
+# Thompsons Construction Tester
+# o = not accept, O = accept
+# ab.cd.|
+#            a     b
+#          o - > o - > o 
+#       e /             \ e
+# - > o                  O
+#       e \             / e
+#          o - > o - > o 
+#            c     d
+#
+#
+#
